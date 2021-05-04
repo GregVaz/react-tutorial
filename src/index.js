275a31bfd87b372cefe1f57f40b06ef548b2618f
Gregorio <gregoriovazya@gmail.com>
2021-05-04 15:48:21 -0600

    Adding props to books

--- src/index.js
+++ src/index.js
@@ -2,33 +2,57 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css'
 
+// setup variables
+const firstBook = {
+  author: 'Josh Hawley',
+  title: 'The Tyranny of Big Tech',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91QrUUK6WQL._AC_UL200_SR200,200_.jpg'
+}
+
+const secondBook = {
+  author: 'Don Miguel Ruiz',
+  title: 'The Four Agreements: A Practical Guide to Personal Freedom',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/91p5b0UgbKL._AC_UL200_SR200,200_.jpg'
+}
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book author={firstBook.author} title={firstBook.title} image={firstBook.img}/>
+      <Book author={secondBook.author} title={secondBook.title} image={secondBook.img}/>
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <Image image={props.image}/>
+      <Title title={props.title}/>
+      <Author author={props.author}/>
     </article>
   );
 }
 
-const Image = () => {
+const Image = ({ image }) => {
   return (
-    <img src="https://images-na.ssl-images-amazon.com/images/I/91QrUUK6WQL._AC_UL200_SR200,200_.jpg" alt=""/>
+    <img src={image} alt=""/>
   );
 }
 
-const Title = () => <h1>The Tyranny of Big Tech</h1>;
-const Author = () => <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Josh Hawley</h4>;
+const Title = ({ title }) => {
+  return (
+    <h1>{title}</h1>
+  );
+}
+
+const Author = ({ author }) => {
+  return (
+    <h4 style={{color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>
+      {author}
+    </h4>
+  );
+}
+
 
 ReactDom.render(<BookList />, document.getElementById('root'));
